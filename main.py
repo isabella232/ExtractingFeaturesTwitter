64b8456198b6646d0e5673e954e8d2d22fe30ab8
Kevin Vuong <kevinvuong99@hotmail.com>
2019-10-07 16:44:18 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,146 @@
+"""
+Note that this lab MUST BE DONE after CleaningTweetData.
+The code below is not par of this current lab ExtractingFeaturesTwitter:
+"""
+import re  # for regular expressions
+import pandas as pd
+
+pd.set_option("display.max_colwidth", 200)
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import string
+import nltk  # for text manipulation
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# training set
+train = pd.read_csv('train.csv')
+# test set
+test = pd.read_csv('test.csv')
+
+""" 
+Preliminary Data Review
+"""
+# Data inspection - non racist/sexist tweets
+print(train[train['label'] == 0].head(10))
+
+# Data inspection - racist/sexist tweets
+print(train[train['label'] == 1].head(10))
+
+# check distribution
+
+length_train = train['tweet'].str.len()
+length_test = test['tweet'].str.len()
+
+plt.hist(length_train, bins=20, label="train_tweets")
+plt.hist(length_test, bins=20, label="test_tweets")
+plt.legend()
+plt.show()
+"""
+Data Cleaning
+"""
+combi = train.append(test, ignore_index=True)
+
+
+def remove_pattern(input_txt, pattern):
+    r = re.findall(pattern, input_txt)
+    for i in r:
+        input_txt = re.sub(i, '', input_txt)
+
+    return input_txt
+
+
+# removing Twitter handles (@user)
+combi['tidy_tweet'] = np.vectorize(remove_pattern)(combi['tweet'], "@[\w]*")
+print(combi.head())
+
+# removing punctuation, numbers, special chars
+combi['tidy_tweet'] = combi['tidy_tweet'].str.replace("[^a-zA-Z#]", " ")
+print(combi.head(10))
+
+# removing short words
+combi['tidy_tweet'] = combi['tidy_tweet'].apply(lambda x: ' '.join([w for w in x.split() if len(w) > 3]))
+print(combi.head())
+
+# text normalization
+tokenized_tweet = combi['tidy_tweet'].apply(lambda x: x.split())  # tokenizing
+print(tokenized_tweet.head())
+
+# normalize tokenized tweets
+
+from nltk.stem.porter import *
+
+stemmer = PorterStemmer()
+tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x])  # stemming
+
+# stitch tokens back together
+for i in range(len(tokenized_tweet)):
+    tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
+
+combi['tidy_tweet'] = tokenized_tweet
+
+"""
+Hashtag Sentiment
+"""
+
+
+# function to collect hashtags
+def hashtag_extract(x):
+    hashtags = []
+    # Loop over the words in the tweet
+    for i in x:
+        ht = re.findall(r"#(\w+)", i)
+        hashtags.append(ht)
+
+    return hashtags
+
+
+# extracting hashtags from non racist/sexist tweets
+
+HT_regular = hashtag_extract(combi['tidy_tweet'][combi['label'] == 0])
+
+# extracting hashtags from racist/sexist tweets
+HT_negative = hashtag_extract(combi['tidy_tweet'][combi['label'] == 1])
+
+# unnesting list
+HT_regular = sum(HT_regular, [])
+HT_negative = sum(HT_negative, [])
+
+# non-racist hashtag visual
+a = nltk.FreqDist(HT_regular)
+d = pd.DataFrame({'Hashtag': list(a.keys()),
+                  'Count': list(a.values())})
+
+# selecting top 20 most frequent hashtags
+d = d.nlargest(columns="Count", n = 20)
+plt.figure(figsize=(16,5))
+ax = sns.barplot(data=d, x= "Hashtag", y = "Count")
+ax.set(ylabel = 'Count')
+plt.show()
+
+# racist hashtag visual
+b = nltk.FreqDist(HT_negative)
+e = pd.DataFrame({'Hashtag': list(b.keys()), 'Count': list(b.values())})
+
+# selecting top 20 most frequent hashtags
+e = e.nlargest(columns="Count", n = 20)
+plt.figure(figsize=(16,5))
+ax = sns.barplot(data=e, x= "Hashtag", y = "Count")
+
+"""
+End of code from CleaningTweetData
+Actual lab starts here
+"""
+
+
+
+
+
+
+
+
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import gensim
 
